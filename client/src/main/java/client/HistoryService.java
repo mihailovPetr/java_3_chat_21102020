1127844b62dfe3c962f2a169790383c52b92654b
vergik <mihailovpeter@gmail.com>
2020-10-28 17:23:57 +0300

    -

--- client/src/main/java/client/HistoryService.java
+++ client/src/main/java/client/HistoryService.java
@@ -1,14 +1,10 @@
 package client;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public interface HistoryService {
 
-    public boolean writeMsg(String msg);
+    boolean writeMsg(String msg);
 
-    public String readMsgs(int n);
+    String readMsgs(int n);
 
-    public void close();
+    void close();
 }
