7fed501c456c4e1362ec00b9c16851829bcd8db7
vergik <mihailovpeter@gmail.com>
2020-10-31 18:00:48 +0300

    -

--- hw4/src/Main.java
+++ hw4/src/Main.java
@@ -1,5 +1,69 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Main {
+    private static char currChar = 'A';
+
+    public static void main(String[] args) {
+        final Object mon = new Object();
+        final int num = 5;
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        service.execute(() -> {
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (mon) {
+                        while (currChar != 'A') {
+                            mon.wait();
+                        }
+                        System.out.println("A");
+                        currChar = 'B';
+                        mon.notifyAll();
+                    }
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        service.execute(() -> {
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (mon) {
+                        while (currChar != 'B') {
+                            mon.wait();
+                        }
+                        System.out.println("B");
+                        currChar = 'C';
+                        mon.notifyAll();
+                    }
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        service.execute(() -> {
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (mon) {
+                        while (currChar != 'C') {
+                            mon.wait();
+                        }
+                        System.out.println("C");
+                        currChar = 'A';
+                        mon.notifyAll();
+                    }
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        service.shutdown();
     }
 }
